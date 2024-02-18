ae07d7b9207a396c20d586cfc0a37976c55021d5
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2024-02-19 02:08:20 +0700

    add comment page

---
+++ comment.php
@@ -0,0 +1,87 @@
+ <div class="album py-5 bg-light">
+    <div class="container">
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+        <div class="col-lg-12">
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-6 align-self-between overflow-hidden">
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="80%" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <p>Disukai oleh...</p>
+                </div>
+                <div class="col-md-6">
+                <div class="card-body">
+                     <p><strong>@username</strong></p>
+            
+                    
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  
+
+                    <div class="row overflow-auto p-3" style="max-width: 100%; max-height: 180px;"> 
+
+                     <div class="col-10 mt-4">
+                     <strong>@username</strong> Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Harum nam nesciunt sint, nihil asperiores aliquam quod neque modi incidunt perferendis dolores fugiat facere perspiciatis voluptate eligendi aut ex. Iure, eum?
+                     </div>   
+
+                     <div class="col-1 align-self-center">
+                     <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn"><i class="bi bi-three-dots"></i></button>
+                     </div>
+                    
+                    <br>
+
+                     <div class="col-10 mt-4">
+                     <strong>@username</strong> Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Harum nam nesciunt sint, nihil asperiores aliquam quod neque modi incidunt perferendis dolores fugiat facere perspiciatis voluptate eligendi aut ex. Iure, eum?
+                     </div>   
+
+                     <div class="col-1 align-self-center">
+                     <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn"><i class="bi bi-three-dots"></i></button>
+                     </div>
+                    
+                    <br>
+                
+
+                    </div>
+
+                    
+
+                      <!-- Modal -->
+                      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered"">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Komentar</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="d-grid gap-2">
+                                <button class="btn btn-danger" type="button">Hapus</button>
+                                <button class="btn btn-light" data-bs-dismiss="modal" type="button">Batal</button>
+                                </div>
+                            </div>
+                        
+                            </div>
+                        </div>
+                        </div>
+
+                    <hr>
+                    <div class="p-2">
+                    <form action="">
+                        <textarea name="" placeholder="Tuliskan komentar Anda disini" id="" class="form-control"></textarea>
+                        <button type="submit" class="btn btn-outline-primary mt-2">Kirim Komentar</button>
+                    </form>
+                    </div>
+                    
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
